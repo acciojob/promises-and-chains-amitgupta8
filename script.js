d317ec20e268eaefdff51f3183cc6473c6edd99b
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-26 23:48:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,36 @@
-function wait(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
-}
-
-function validateAge(age) {
-  return new Promise((resolve, reject) => {
-    if (age >= 18) {
-      resolve();
-    } else {
-      reject();
-    }
-  });
-}
-
-function extractNameAndCreateObject() {
-  const name = document.getElementById("name").value;
-  const newObj = { name };
-  return newObj;
-}
-
-function showAlert(obj) {
-  alert(`Welcome, ${obj.name}. You can vote.`);
-}
-
-function showError(obj) {
-  alert(`Oh sorry ${obj.name}. You aren't old enough.`);
-}
-
-const form = document.querySelector("form");
-form.addEventListener("submit", async event => {
-  event.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const age = document.getElementById("age").value;
-
-  if (!name || !age) {
-    alert("Please fill in all fields.");
-    return;
-  }
-
-  try {
-    await wait(4000);
-    await validateAge(age);
-    const obj = extractNameAndCreateObject();
-    showAlert(obj);
-  } catch (error) {
-    const obj = extractNameAndCreateObject();
-    showError(obj);
-  }
-});
+const form = document.querySelector('#myForm'); 
+   const nameInput = document.querySelector('#name'); 
+   const ageInput = document.querySelector('#age'); 
+   const btn = document.querySelector('#btn'); 
+  
+   form.addEventListener('submit', (event) => { 
+     event.preventDefault(); // Prevent form from submitting normally 
+  
+     if (nameInput.value === '' || ageInput.value === '') { 
+       alert('Please fill out all fields.'); // Show validation error 
+       return; 
+     } 
+  
+     const age = parseInt(ageInput.value); 
+  
+     // Check if age is above 18 after 4 seconds 
+     const promise = new Promise((resolve, reject) => { 
+       setTimeout(() => { 
+         if (age >= 18) { 
+           resolve(); 
+         } else { 
+           reject(); 
+         } 
+       }, 4000); 
+     }); 
+  
+     promise 
+       .then(() => { 
+         // Show welcome message if age is above 18 
+         alert(`Welcome, ${nameInput.value}. You can vote.`); 
+       }) 
+       .catch(() => { 
+         // Show rejection message if age is below 18 
+         alert(`Oh sorry ${nameInput.value}. You aren't old enough.`); 
+       }); 
+   });
